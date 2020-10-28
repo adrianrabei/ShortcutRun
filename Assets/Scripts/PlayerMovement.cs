81f6738305e088c5e4d02c63ab870569f8578529
Adrian <59284453+adrianrabei@users.noreply.github.com>
2020-10-28 15:14:27 +0200

    Player controll basics

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody player;
+    [SerializeField] private Camera cam;
+    [SerializeField] private float moveSpeed = 10f;
+    
+    void Start()
+    {
+        player = GetComponent<Rigidbody>();
+        
+        
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+
+        if (Input.GetMouseButton(0))
+        {
+            float rotationSpeed = Input.GetAxis("Mouse X") * 5f;
+            transform.RotateAround(transform.position, Vector3.up, rotationSpeed);
+        }
+
+    }
+
+}
