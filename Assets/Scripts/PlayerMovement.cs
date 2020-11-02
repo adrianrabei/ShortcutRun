bf7b0499b1c51c599a352c4992643875e32c308d
Adrian <59284453+adrianrabei@users.noreply.github.com>
2020-11-02 14:40:53 +0200

    Jump fix, planc walking fix

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,15 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody player;
     [SerializeField] private Camera cam;
-    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float moveSpeed = 10f, jumpForce = 10f;
     [SerializeField] private GameObject plancPrefab;
     [SerializeField] private int plancCount;
     [SerializeField] private GameObject plancPos;
-    private bool canSpawn;
+    private bool canSpawn, canJump, isPlanc, isFirstPlanc, jumped, onGround, onPlanc;
+    private RaycastHit hit;
+    [SerializeField] private GameObject rayOriginPos;
+    private float plancDistance;
+    private GameObject currentPlancPos;
 
     void Start()
     {
@@ -26,33 +30,60 @@ public class PlayerMovement : MonoBehaviour
             float rotationSpeed = Input.GetAxis("Mouse X") * 5f;
             player.transform.RotateAround(player.transform.position, Vector3.up, rotationSpeed);
         }
-
-        
-
     }
 
-    private void OnCollisionExit(Collision collision)
+    private void Update()
     {
-        canSpawn = true;
-        if(plancCount != 0)
+        if(onGround || onPlanc)
         {
-            Debug.Log("SET PLANC");
-            SetPlanc();
+            GroundCheck();
+        }
+    }
+
+
+
+    private void GroundCheck()
+    {
+        if (Physics.Raycast(rayOriginPos.transform.position, Vector3.down, out hit) && hit.transform.tag == "Ground" || hit.transform != null && hit.transform.tag == "Planc")
+        {
+           
         }
         else
         {
-            Debug.Log("No more contact with " + collision.transform.name);
-            player.AddForce(Vector3.up * 5, ForceMode.Impulse);
-            Falling();
+            if (plancCount != 0)
+            {
+                if (!isFirstPlanc)
+                {
+                    SetPlanc();
+                    isFirstPlanc = true;
+                    return;
+                }
+                if (isFirstPlanc)
+                {
+                    plancDistance = Vector3.Distance(currentPlancPos.transform.position, plancPos.transform.position);
+                    if (plancDistance > 1)
+                    {
+                        SetPlanc();
+                    }
+                }
+            }
+            else Jump();
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        canSpawn = false;
+        if(collision.transform.tag == "Ground")
+        {
+            onGround = true;
+        }
+        if(collision.transform.tag == "Planc")
+        {
+            onPlanc = true;
+        }
     }
 
-    public void Falling()
+    public void GravityAmplification()
     {
         if(player.velocity.y < 0)
         {
@@ -60,13 +91,22 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    public void SetPlanc()
+    private void SetPlanc()
     {
-        if(canSpawn)
+        if (plancCount != 0)
         {
-            Instantiate(plancPrefab, plancPos.transform.position, plancPos.transform.rotation);
-            //plancCount--;
+            currentPlancPos = Instantiate(plancPrefab, plancPos.transform.position, plancPos.transform.rotation);
+            plancCount--;
         }
+    }
+
+    private void Jump()
+    {
+        Debug.Log("JUMP");
+        player.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        GravityAmplification();
+        onGround = false;
+        onPlanc = false;
     }
 
 }
